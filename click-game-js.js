bdbcdcbd5893f9872dd43e3bda14cdebcd8bbc4a
Mark-A-A <markawai01@gmail.com>
2015-11-18 23:25:25 -0500

    added images and corrected sizing with CSS

--- click-game-js.js
+++ click-game-js.js
@@ -1,27 +1,46 @@
 
 var clickCount;
 var startButton = document.getElementById("start");
+//var startButtonClass = document.getElementsByClass("btn btn-success");
 var imageElements = document.getElementById("image");
 // var stopButton =document.getElementsByID("stop");
+
+
+//Timer Variables
+var secondsCount;
+var timerInterval;
+var randomTime;
+
+function addSeconds () {    //Counter "addSeconds"
+  secondsCount++;
+}; //End of function
+
+
+//Timer Button with Start and Stop
+function timer (){
+  if( this.getAttribute("data-state") === "start"){
+    alert("Starting Timer NOW!");
+  } else if (this.getAttribute("data-state") === "stop") {
+    alert("Stopping Timer NOW")
+  }; // End of if statement
+}; // End of function
+
 
 function counter (){
   clickCount++;
 }; 
 
 
-
 function clicker () {
   for (var i = 0; i < imageElements.length; i++) {
     clickCount++;
     alert("Clicked!");
- };
-  
-//};
+ }
+};
 
 
-function gameStart () {
-  
-  alert("Start!");
+function gameStart () { 
+alert("Start!");
 };
 
 // function gameStop (){
@@ -35,8 +54,8 @@ function gameStart () {
 
 
 //Add event Listener for start button
-
-//Add event listener for clicker
+//startButton.addEventListener("click", timer);
 startButton.addEventListener("click", gameStart);
-//imageElements.addEventListener("click", clicker);
+//Add event listener for clicker
+//imageElements.addEventListener("click", counter);
 //
